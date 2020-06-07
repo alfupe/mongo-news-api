720bf4894eece7edac5b0538469672a9107a8631
Alex Fuentes <alfupe@gmail.com>
2020-06-07 14:31:56 +0200

    deprecation warning de mongoose

--- app/models/index.js
+++ app/models/index.js
@@ -2,6 +2,9 @@ const dbConfig = require('../config/db.config');
 const mongoose = require('mongoose');
 mongoose.Promise = global.Promise;
 
+/** @see https://mongoosejs.com/docs/deprecations.html#findandmodify */
+mongoose.set('useFindAndModify', false);
+
 const db = {};
 db.mongoose = mongoose;
 db.url = dbConfig.url;
